e7a9b1c518e5575dba7fdd0db721acb0631862bd
Ajay <ajaymungur@hotmail.com>
2020-06-15 16:36:30 +0100

    Added further styling and screen will now scroll to the top when notification renders

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Router, Route } from 'react-router-dom'
 import Home from './home'
 import history from './history'
 
+import "./stylesheets/main.css"
+
 class App extends React.Component {
   render(){
     return(
--- src/notification/notification.js
+++ src/notification/notification.js
@@ -10,6 +10,7 @@ class Notification extends React.Component{
   buildNotification = () => {
     const { message, type } = this.props.notificationState;
     const { unsetNotification } = this.props
+    document.documentElement.scrollTop = 0;
     return(
       <div className={`notification ${type}`}>
         {timedFunc(unsetNotification, 2000)}
@@ -22,8 +23,6 @@ class Notification extends React.Component{
     const { show } = this.props.notificationState;
     return show ? this.buildNotification() : null;
   }
-
-
 
   render(){
     return(
